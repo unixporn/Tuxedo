309927e28b3555967617defd647b7a449a814c55
eye-sigil <ustin.a.h+github@gmail.com>
2018-02-18 20:13:36 -0500

    Added youtube

---
+++ extensions/youtube.py
@@ -0,0 +1,28 @@
+from urllib import parse
+
+import aiohttp
+from bs4 import BeautifulSoup
+from discord.ext import commands
+
+
+class Youtube:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['yt', 'vid', 'video'])
+    async def youtube(self, ctx, query: str):
+        """Search for videos on YouTube"""
+        search = parse.quote(query)
+        async with aiohttp.ClientSession() as session:
+            response = await session.get(
+                f"https://www.youtube.com/results?search_query={search}",
+                verify_ssl=False).text()
+        result = BeautifulSoup(response, "html.parser")
+        await ctx.send("https://www.youtube.com{}".format(
+            result.find_all(attrs={'class': 'yt-uix-tile-link'})[0]
+                  .get('href')))
+
+
+def setup(bot):
+    bot.add_cog(Youtube(bot))
