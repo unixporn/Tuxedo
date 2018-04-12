39fc1774fb0b516660564b53c8e0d029b197bd3b
eye-sigil <ustin.a.h+github@gmail.com>
2018-04-12 15:45:16 -0400

    Made that a thing

--- extensions/search.py
+++ extensions/search.py
@@ -14,8 +14,8 @@ class Search:
         with open('searxes.txt') as instances:
             instance = random.sample(instances.read().split('\n'), k=1)
         call = f'https://{instance[0]}/search?q={query}&format=json'
-        await ctx.send(call)
-        response = await (await self.session.get(call)).json()
+        async with session.get('call') as resp:
+            response = await resp.json()
 
         # infoboxes = response['infoboxes']
         results = response['results']
