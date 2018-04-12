441c67f0570710b03471cdd1c4def109ee27f860
eye-sigil <ustin.a.h+github@gmail.com>
2018-04-12 15:31:46 -0400

    New search cog

---
+++ extensions/search.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import aiohttp
+import random
+
+
+class Search:
+    def __init__(self, bot):
+        self.conn = bot.conn
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
+
+    @commands.command()
+    async def search(self, ctx, *, query: str):
+        with open('searxes.txt') as instances:
+            instance = random.sample(instances.read().split('\n'), k=1)
+        call = f'https://{instance}/search?q={query}&format=json'
+        response = (await self.session.get(call)).json()
+
+        # infoboxes = response['infoboxes']
+        results = response['results']
+
+        msg = f"Showing **5** results for `{query}`. \n\n"
+        msg += (f"**{results[0]['title']}** <{results[0]['url']}>\n"
+                f"{results[0]['content']}\n\n")
+        msg += "\n".join(
+            [f'**{entry.title}** <{entry.url}>' for entry in results[1:4]])
+        msg += f"\n_Results retrieved from instance `{instance}`._"
+
+        await ctx.send(msg)
+
+
+def setup(bot):
+    bot.add_cog(Search(bot))
