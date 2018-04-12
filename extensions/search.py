152ae080699df4c44e208db0e1f80a94a05ded9c
eye-sigil <ustin.a.h+github@gmail.com>
2018-04-12 16:02:30 -0400

    Watashi wa DONE!

--- extensions/search.py
+++ extensions/search.py
@@ -14,18 +14,28 @@ class Search:
         with open('searxes.txt') as instances:
             instance = random.sample(instances.read().split('\n'), k=1)
         call = f'https://{instance[0]}/search?q={query}&format=json'
-        async with self.session.get(call) as resp:
-            response = await resp.json()
+        try:
+            async with self.session.get(call) as resp:
+                response = await resp.json()
+        except aiohttp.ClientConnectorError:
+            await ctx.send(
+                f"There was a problem with `{instance[0]}`. Please contact "
+                f"ry00001#3487 to have it removed.")
 
         # infoboxes = response['infoboxes']
         results = response['results']
 
-        msg = f"Showing **5** results for `{query}`. \n\n"
-        msg += (f"**{results[0]['title']}** <{results[0]['url']}>\n"
-                f"{results[0]['content']}\n\n")
-        msg += "\n".join(
-            [f"**{entry['title']}** <{entry['url']}>" for entry in results[1:5]])
-        msg += f"\n\n_Results retrieved from instance `{instance[0]}`._"
+        try:
+            msg = f"Showing **5** results for `{query}`. \n\n"
+            msg += (f"**{results[0]['title']}** <{results[0]['url']}>\n"
+                    f"{results[0]['content']}\n\n")
+            msg += "\n".join(
+                [f"**{entry['title']}** <{entry['url']}>" for entry in results[1:5]])
+            msg += f"\n\n_Results retrieved from instance `{instance[0]}`._"
+        except:
+            await ctx.send(
+                f"There was a problem with `{instance[0]}`. Please contact "
+                f"ry00001#3487 to have it removed.")
 
         await ctx.send(msg)
 
